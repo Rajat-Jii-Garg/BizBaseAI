b8d26d202b0f26f075b6584df3c4bf649a7e7f5a
Rajat Garg <97934406+Rajat-Jii-Garg@users.noreply.github.com>
2025-09-07 00:09:36 +0530

    update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import { Link } from "react-router-dom";
 import { Card, CardContent } from '@/components/ui/card';
 import { Star, Crown, TrendingUp, DollarSign } from 'lucide-react';
 
