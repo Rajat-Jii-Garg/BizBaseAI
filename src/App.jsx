ace81b8a574be3c71cfa6099deffdfdce4c4a497
Rajat-Jii-Garg <rajatjiigarg1@gmail.com>
2025-09-06 13:13:43 +0530

    update network

--- src/App.jsx
+++ src/App.jsx
@@ -45,6 +45,7 @@ const App = () => (
             <Route path="/auth" element={<Auth />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
+            <Route path="/network" element={<Network />} />
             <Route path="/dashboard" element={
               <ProtectedRoute>
                 <Dashboard />
